469bc7b0729a2a0f888659bc8e55c46638f43648
y-Mutawakil <myussif020@st.ug.edu.gh>
2024-05-24 17:13:34 +0000

    Adding App.js with Changed Background Color

---
+++ App.js
@@ -0,0 +1,16 @@
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor:"#121912"
+      }}
+    >
+      <Text style={{color:"white"}}>Universal React with Expo</Text>
+    </View>
+  );
+}
